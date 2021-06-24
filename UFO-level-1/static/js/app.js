c3d69ce37bf25fb9499d4e688785ad58c4ef94c3
miller78130 <miller78130@gmail.com>
2021-06-23 22:14:52 -0500

    ufo-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,39 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+var tbody = d3.select("tbody");
+
+
+// Create event handlers 
+button.on("click", runEnter);
+//form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  var inputElement = d3.select("#datetime");
+
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  //console.log(typeof(inputValue));
+
+  var filteredData = tableData.filter(siting => siting.datetime === inputValue);
+
+  console.log(filteredData);
+  
+filteredData.forEach(function(siting) {
+    var row = tbody.append("tr");
+    Object.entries(siting).forEach(function([key, value]) {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+};
